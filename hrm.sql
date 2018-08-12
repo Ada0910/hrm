15a6107b2fc1962f3b082ec5d972cd3620962a82
weining <1355948107@qq.com>
2018-08-12 20:40:56 +0800

    人事管理系统

---
+++ hrm.sql
@@ -0,0 +1,145 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.21 : Database - hrm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hrm` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `hrm`;
+
+/*Table structure for table `dept_inf` */
+
+DROP TABLE IF EXISTS `dept_inf`;
+
+CREATE TABLE `dept_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  `REMARK` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+/*Data for the table `dept_inf` */
+
+insert  into `dept_inf`(`ID`,`NAME`,`REMARK`) values (1,'策划部','策划部'),(2,'产品部','产品部'),(3,'技术部','技术部'),(4,'运营部','运营部'),(5,'市场部','市场部'),(6,'财务部','财务部'),(7,'教学部','教学部');
+
+/*Table structure for table `document_inf` */
+
+DROP TABLE IF EXISTS `document_inf`;
+
+CREATE TABLE `document_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TITLE` varchar(50) NOT NULL,
+  `filename` varchar(300) NOT NULL,
+  `REMARK` varchar(300) DEFAULT NULL,
+  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `USER_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_DOCUMENT_USER` (`USER_ID`),
+  CONSTRAINT `FK_DOCUMENT_USER` FOREIGN KEY (`USER_ID`) REFERENCES `user_inf` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `document_inf` */
+
+insert  into `document_inf`(`ID`,`TITLE`,`filename`,`REMARK`,`CREATE_DATE`,`USER_ID`) values (1,'Java入门到放弃','Java入门到精通','全国优秀畅销书','2018-07-31 16:09:31',1);
+
+/*Table structure for table `employee_inf` */
+
+DROP TABLE IF EXISTS `employee_inf`;
+
+CREATE TABLE `employee_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DEPT_ID` int(11) NOT NULL,
+  `JOB_ID` int(11) NOT NULL,
+  `NAME` varchar(20) NOT NULL,
+  `CARD_ID` varchar(18) NOT NULL,
+  `ADDRESS` varchar(50) NOT NULL,
+  `POST_CODE` varchar(50) DEFAULT NULL,
+  `TEL` varchar(16) DEFAULT NULL,
+  `PHONE` varchar(11) NOT NULL,
+  `QQ_NUM` varchar(10) DEFAULT NULL,
+  `EMAIL` varchar(50) NOT NULL,
+  `SEX` int(11) NOT NULL DEFAULT '1',
+  `PARTY` varchar(10) DEFAULT NULL,
+  `BIRTHDAY` datetime DEFAULT NULL,
+  `RACE` varchar(100) DEFAULT NULL,
+  `EDUCATION` varchar(10) DEFAULT NULL,
+  `SPECIALITY` varchar(20) DEFAULT NULL,
+  `HOBBY` varchar(100) DEFAULT NULL,
+  `REMARK` varchar(500) DEFAULT NULL,
+  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  KEY `FK_EMP_DEPT` (`DEPT_ID`),
+  KEY `FK_EMP_JOB` (`JOB_ID`),
+  CONSTRAINT `FK_EMP_DEPT` FOREIGN KEY (`DEPT_ID`) REFERENCES `dept_inf` (`ID`),
+  CONSTRAINT `FK_EMP_JOB` FOREIGN KEY (`JOB_ID`) REFERENCES `job_inf` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+/*Data for the table `employee_inf` */
+
+insert  into `employee_inf`(`ID`,`DEPT_ID`,`JOB_ID`,`NAME`,`CARD_ID`,`ADDRESS`,`POST_CODE`,`TEL`,`PHONE`,`QQ_NUM`,`EMAIL`,`SEX`,`PARTY`,`BIRTHDAY`,`RACE`,`EDUCATION`,`SPECIALITY`,`HOBBY`,`REMARK`,`CREATE_DATE`) values (1,3,8,'野猪阿迪','15251102237','陆河','510000','020-77777777','13433237869','876750619\r','876750619@qq.com',1,'预备党员','1997-11-15 00:00:00','汉','本科','计算机','唱歌','野猪阿迪,辣鸡经理','2018-07-31 16:19:56'),(2,3,7,'色长','15251102224','潮州','42427424','020-14154144','15919544267','1055198286','1055198286@qq.com',1,'团员','1996-08-11 00:00:00','汉','本科','计算机','玩游戏','超越小迷弟','2018-07-31 16:19:56'),(3,4,7,'小黑','15251102209','陆丰','510000','020-99999999','15112042748','349617737\r','349617737@qq.com',1,'党员','1996-05-08 00:00:00','汉','本科','计算机','爬山','大黑鬼','2018-07-31 16:19:56'),(4,3,2,'啊哒','15251102250','揭阳','515550','020-56545454','13162323558','1355948107','1355948107@qq.com',1,'团员','1996-09-10 00:00:00','汉','本科','计算机','睡觉','傻逼哒','2018-07-31 16:19:56');
+
+/*Table structure for table `job_inf` */
+
+DROP TABLE IF EXISTS `job_inf`;
+
+CREATE TABLE `job_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  `REMARK` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+/*Data for the table `job_inf` */
+
+insert  into `job_inf`(`ID`,`NAME`,`REMARK`) values (1,'职员','职员'),(2,'Java开发工程师','Java开发工程师'),(3,'Java中级开发工程师','Java中级开发工程师'),(4,'Java高级开发工程师','Java高级开发工程师'),(5,'系统管理员','系统管理员'),(6,'架构师','架构师'),(7,'主管','主管'),(8,'经理','经理'),(9,'总经理','总经理');
+
+/*Table structure for table `notice_inf` */
+
+DROP TABLE IF EXISTS `notice_inf`;
+
+CREATE TABLE `notice_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TITLE` varchar(50) NOT NULL,
+  `CONTENT` text NOT NULL,
+  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `USER_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_NOTICE_USER` (`USER_ID`),
+  CONSTRAINT `FK_NOTICE_USER` FOREIGN KEY (`USER_ID`) REFERENCES `user_inf` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+/*Data for the table `notice_inf` */
+
+insert  into `notice_inf`(`ID`,`TITLE`,`CONTENT`,`CREATE_DATE`,`USER_ID`) values (1,'小辣椒','我是小辣椒，噢耶','2018-08-10 17:21:14',2),(20,'潮汕电影上映了','再过两个星期就是万众期待的电影上映了','2018-08-11 17:32:48',1);
+
+/*Table structure for table `user_inf` */
+
+DROP TABLE IF EXISTS `user_inf`;
+
+CREATE TABLE `user_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `loginname` varchar(20) NOT NULL,
+  `PASSWORD` varchar(16) NOT NULL,
+  `STATUS` int(11) NOT NULL DEFAULT '1',
+  `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_inf` */
+
+insert  into `user_inf`(`ID`,`loginname`,`PASSWORD`,`STATUS`,`createdate`,`username`) values (1,'ada','ada',1,'2018-08-02 16:26:31','超级管理员Ada'),(2,'paprika','123',1,'2018-08-09 15:32:53','色长');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
